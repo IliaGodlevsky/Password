be0fad55e82e7df85960a47c8a3a356b059beaa9
IliaGodlevsky <godleevsky2015@gmail.com>
2019-07-03 18:01:06 +0300

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -3,7 +3,6 @@
 #define CONSTANTS_H_
 #include <vector>
 #include "Functions.h"
-enum { LETTERS, NUMBERS, SYMBOLS };
 static const int MIN_LENGTH = 6;	// WARNING: do not make it equal less than 4. Endless loop alert!
 static const int MAX_LENGTH = 25;
 static const int MODES = 3;
