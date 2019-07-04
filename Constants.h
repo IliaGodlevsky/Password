b759b7a600f77be1cbf87c858a7f307897db2609
IliaGodlevsky <godleevsky2015@gmail.com>
2019-07-04 12:18:13 +0300

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -6,23 +6,23 @@ static const int MAX_LENGTH = 18;
 static const int MODES = 7;
 static const int SYMBOLS_TYPES = 4;
 static const int EXAMPLES = 25;
-static const is_char status[MODES] = { 
+static const is_char status[MODES] = {
 	isalpha,
-	isdigit, 
+	isdigit,
 	issymbol,
 	isdigit_or_letter,
 	isletter_or_symbol,
 	isdigit_or_symbol,
-	isletter_or_digit_or_symbol 
+	isletter_or_digit_or_symbol
 };
-static const has_char has[MODES] = { 
-	has_letters, 
+static const has_char has[MODES] = {
+	has_letters,
 	has_digits,
 	has_symbols,
 	has_digits_and_letters,
 	has_letters_and_symbols,
 	has_digits_and_symbols,
-	has_symbols_digits_and_letters 
+	has_symbols_digits_and_letters
 };
 static const char* lower_letters = "abcdefghijklmnopqrstuvwxyz";
 static const char* upper_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
